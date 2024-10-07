47e9e8eb8558d2456a57917f732ab318fc75baab
aaditmuley13 <muleyaadit@gmail.com>
2024-10-07 09:28:19 +0530

    Update AboutPyHack.js

--- src/AboutPyHack.js
+++ src/AboutPyHack.js
@@ -6,14 +6,14 @@ function AboutPyHack() {
     <section className="about-pyhack">
       <h2>About PyHack</h2>
       <p>PyHack is a dynamic, community-driven workshop designed to teach game development using Python.</p>
-      <a href="#" className="learn-more">Learn More &rarr;</a>
+      <a className="learn-more">Learn More &rarr;</a>
 
       <div className="about-grid">
         <div className="about-card">
-        <img src={smiley} alt="Placeholder Image" />
+        <img src={smiley}  />
         </div>
         <div className="about-card">
-          <img src={tt} alt="Placeholder Image" />
+          <img src={tt}  />
 
           
         </div>
